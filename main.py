440cf1ac8225abdae90d6a113e8966455ae65cd4
schoibs <choibingshen2001@gmail.com>
2022-12-04 20:33:48 +0800

    Initialize virtual environment

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
-import requests
-from bs4 import BeautifulSoup
+from fastapi import FastAPI, Path
 import cloudscraper
+from bs4 import BeautifulSoup
 
-ticker = 'MSFT'
-scraper = cloudscraper.create_scraper(delay=10, browser={'custom': 'ScraperBot/1.0'})
+app = FastAPI()
+scraper = cloudscraper.create_scraper(
+    delay=10, browser={'custom': 'ScraperBot/1.0'})
 
-url = f'https://finviz.com/quote.ashx?t={ticker}'
-response = scraper.get(url)
 
-soup = BeautifulSoup(response.content, 'lxml')
+@app.get("/")
+def getData():
+    return 'Hello World! This application works.'
 
-table_rows = soup.select('.snapshot-table2 .table-dark-row')
-temp_list = []
-result_dict = {}
 
-for row in table_rows:
-    row_cells = row.find_all('td')
-    
-    for i in range(len(row_cells)):
-        text = row_cells[i].text
-        temp_list.append(text)
-        
-        if i % 2 > 0:
-            result_dict[temp_list[0]] = temp_list[1]
-            temp_list.clear()
+@app.get("/stocks/{ticker}")
+def getStocksData(ticker: str = Path('AMC', description='Description yoyo')):
+    # ticker = 'MSFT'
 
-print(result_dict)
+    url = f'https://finviz.com/quote.ashx?t={ticker}'
+    response = scraper.get(url)
+    soup = BeautifulSoup(response.content, 'lxml')
+
+    table_rows = soup.select('.snapshot-table2 .table-dark-row')
+    temp_list = []
+    result_dict = {}
+
+    for row in table_rows:
+        row_cells = row.find_all('td')
+
+        for i in range(len(row_cells)):
+            text = row_cells[i].text
+            temp_list.append(text)
+
+            if i % 2 > 0:
+                result_dict[temp_list[0]] = temp_list[1]
+                temp_list.clear()
+
+    return result_dict
